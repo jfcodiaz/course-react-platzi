c94d7135a72e203861212ae9cdcd0e3e48cfc96c
JFco Diaz <jfcodiaz@gmail.com>
2021-05-20 05:44:46 +0000

    import styles and first props

--- code/platzi-badges/src/components/Badge.js
+++ code/platzi-badges/src/components/Badge.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
 import confLogo from '../images/badge-header.svg';
+import "./styles/Badge.css";
 
 class Badge extends React.Component {
-    render() {
+    render() {        
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Confrerence logo"/>                    
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-                    <h1>Pako<br/>Díaz</h1>
+                <div className="Badge__section-name">
+                    <img 
+                        className="Badge__avatar"
+                        src={this.props.avatar} alt="Avatar"/>
+                    <h1>{this.props.firstName}<br/>{this.props.lastName}</h1>
                 </div>
-                <div>
-                    <p>Fullstack Enginner</p>
-                    <p>@fcodiaz</p>
+                <div className="Badge__section-info">
+                    <h3>{this.props.jobTitle}</h3>
+                    <div>{this.props.twitter}</div>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
--- code/platzi-badges/src/index.js
+++ code/platzi-badges/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
 import Badges from './components/Badge';
 
-const container = document.getElementById('app');
+const container = document.getElementById('app'); 
 
-ReactDOM.render(<Badges/>, container);
+ReactDOM.render(
+    <Badges 
+        firstName="Pako" 
+        lastName="Díaz"
+        jobTitle="Fullstack engineer"
+        twitter="@pakodiaz"
+        avatar="https://www.gravatar.com/avatar?d=identicon"
+    />, container
+);
